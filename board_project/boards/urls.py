b79d924ac0b4b4361b17ae515a6b25272119379f
hokagequan <Q@Quans-MacBook.local>
2018-01-29 23:52:02 +0800

    new topic

--- board_project/boards/urls.py
+++ board_project/boards/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name="home"),
-	path('boards/<pk>', views.board_topics, name="board_topics"),
+	path('boards/<pk>/', views.board_topics, name="board_topics"),
+	path('boards/<pk>/new/', views.new_topic, name="new_topic"),
 ]
