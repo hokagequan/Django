4543af8c811d103c984cf40f267f50b5bd359b56
hokagequan <Q@Quans-MacBook.local>
2018-01-29 23:40:24 +0800

    html

---
+++ board_project/boards/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('', views.home, name="home"),
+	path('boards/<pk>', views.board_topics, name="board_topics"),
+]
--- board_project/boards/views.py
+++ board_project/boards/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Board
 
 # Create your views here.
 def home(request):
-	return HttpResponse("Hello World!")
+	boards = Board.objects.all()
+	return render(request, "boards.html", {"boards": boards})
+
+def board_topics(request, pk):
+	board = get_object_or_404(Board, pk=pk)
+	return render(request, "topics.html", {"board": board})
