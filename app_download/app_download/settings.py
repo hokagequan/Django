9358fb71b9a38b62e4f41a8e0fe99a4fa8db530f
quanchengwen <quanchengwen@9rush.com>
2018-02-05 15:26:03 +0800

    目录结构

--- app_download/app_download/settings.py
+++ app_download/app_download/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'app_download.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "templates")
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
